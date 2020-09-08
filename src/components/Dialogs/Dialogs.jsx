5a76e2f2612505979385a01c9df3255010dfc5f5
SunR1sen <sunr1sen.dev@gmail.com>
2020-09-09 00:21:09 +0300

    Lesson 21 added Messages skeleton

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Diman</div>
+                <div className={s.dialog}>Marina</div>
+                <div className={s.dialog}>Morti</div>
+                <div className={s.dialog}>Kabluchki</div>
+                <div className={s.dialog}>Makiyaj</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Pidarok</div>
+                <div className={s.message}>Jresh govno?</div>
+            </div>
         </div>
     )
 }
