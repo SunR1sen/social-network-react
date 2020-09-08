473bfe931ba85cd2583f821dc4671f1874d44bdc
SunR1sen <sunr1sen.dev@gmail.com>
2020-09-08 22:26:10 +0300

    Lesson 19 added NavLink and Route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <Router>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Switch>
+                        <Route path='/dialogs'>
+                            <Dialogs />
+                        </Route>
+                        <Route path='/profile'>
+                            <Profile />
+                        </Route>
+                        <Route path='/news'>
+                            <News />
+                        </Route>
+                        <Route path='/music'>
+                            <Music />
+                        </Route>
+                        <Route path='/settings'>
+                            <Settings />
+                        </Route>
+                    </Switch>
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>
+        </Router>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <ul className={s.nav__menu}>
-                <li><a  className={`${s.nav__link} ${s.active}`} href="#">Profile</a></li>
-                <li><a  className={s.nav__link} href="#">Messages</a></li>
-                <li><a  className={s.nav__link} href="#">News</a></li>
-                <li><a  className={s.nav__link} href="#">Music</a></li>
-                <li><a  className={s.nav__link} href="#">Settings</a></li>
+                <li><NavLink className={s.nav__link} activeClassName={s.active} to="/profile">Profile</NavLink></li>
+                <li><NavLink className={s.nav__link} activeClassName={s.active} to="/dialogs">Messages</NavLink></li>
+                <li><NavLink className={s.nav__link} activeClassName={s.active} to="/news">News</NavLink></li>
+                <li><NavLink className={s.nav__link} activeClassName={s.active} to="/music">Music</NavLink></li>
+                <li><NavLink className={s.nav__link} activeClassName={s.active} to="/settings">Settings</NavLink></li>
             </ul>
         </nav>
     )
