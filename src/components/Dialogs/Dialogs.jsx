1b3eea25884cc9c129cd924d330e8310276acf1c
SunR1sen <sunr1sen.dev@gmail.com>
2020-09-10 12:29:19 +0300

    Lesson 25 added DialogItem and Message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    return (
-        <div className={s.dialog}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
+import DialogItem from "./DIalogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = () => {
     const dialogs = [
