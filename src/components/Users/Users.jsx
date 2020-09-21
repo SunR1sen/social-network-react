b05d7c0fc22b6d879ae71962ba163897fe208602
SunR1sen <sunr1sen.dev@gmail.com>
2020-09-21 11:42:26 +0300

    Added axios and server API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,48 @@
 import React from 'react';
 import s from './Users.module.scss'
+import * as axios from 'axios';
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/batman-icon.png',
-                followed: true,
-                fullName: 'Dmitry',
-                status: "Im a first user",
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/batman-icon.png',
-                followed: false,
-                fullName: 'Marina',
-                status: "Im a second user",
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/batman-icon.png',
-                followed: true,
-                fullName: 'Morti',
-                status: "I have a tail",
-                location: {city: 'Moscow', country: 'Russia'}
-            }
-        ])
-    }
+    // if (props.users.length === 0) {
+    //     props.setUsers([
+    //         {
+    //             id: 1,
+    //             photoUrl: '',
+    //             followed: true,
+    //             fullName: 'Dmitry',
+    //             status: "Im a first user",
+    {/*            location: {city: 'Moscow', country: 'Russia'}*/}
+    {/*        },*/}
+    //         {
+    //             id: 2,
+    //             photoUrl: '',
+    //             followed: false,
+    //             fullName: 'Marina',
+    //             status: "Im a second user",
+    //             location: {city: 'Moscow', country: 'Russia'}
+    //         },
+    {/*        {*/}
+    {/*            id: 3,*/}
+    //             photoUrl: '',
+    //             followed: true,
+    //             fullName: 'Morti',
+    //             status: "I have a tail",
+    //             location: {city: 'Moscow', country: 'Russia'}
+    //         }
+    //     ])
+    // }
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+            debugger;
+            props.setUsers(response.data.items)
+        })
 
     return <div>
         {props.users.map(u => <div key={u.id} className={s.userWrapper}>
             <div className={s.photoContainer}>
-                <img className={s.userPhoto} src={u.photoUrl}/>
+                <img className={s.userPhoto} src={u.photoUrl} alt='Photo'/>
                 {u.followed
                     ? <button className={s.subBtn} onClick={() => props.unfollow(u.id)}>Отписаться</button>
                     : <button className={s.subBtn} onClick={() => props.follow(u.id)}>Подписаться</button>
@@ -44,14 +51,14 @@ const Users = (props) => {
             </div>
             <div className={s.infoContainer}>
                 <div className={s.leftSide}>
-                    <span className={s.userName}>{u.fullName}</span>
+                    <span className={s.userName}>{u.name}</span>
                     <p className={s.status}>{u.status}</p>
 
                 </div>
                 <div className={s.rightSide}>
                     <div className={s.location}>
-                        <p>{u.location.city}</p>
-                        <p>{u.location.country}</p>
+                        <p>Gorod</p>
+                        <p>Strana</p>
                     </div>
 
                 </div>
